8d86bed74da17fc177dd3b619c592dc1841dfab4
Chandra Mohan Reddy Veeramreddy <33657970+cmreddyv@users.noreply.github.com>
2020-02-11 17:17:31 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo 'Installing ActiveMQ Prereqs...'
 sudo yum install update -y
 sudo yum install java-1.8.0-openjdk -y
-#sudo curl "https://bootstrap.pypa.io/get-pip.py" | python3
-#sudo pip3 install -r requirements.txt
+sudo curl "https://bootstrap.pypa.io/get-pip.py" | python3
+sudo pip3 install -r requirements.txt
 echo '---------------------------------------------------'
 echo 'Install complete! run: python ActiveMQ.py to start'
